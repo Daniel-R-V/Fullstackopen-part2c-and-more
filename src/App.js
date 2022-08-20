f45861de6dd064a89dbbbf753f1587f00389d249
Daniel <danirvicente@gmail.com>
2022-08-20 20:17:11 +0200

    fix imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
