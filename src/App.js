4465feda1ac22d13078f5392561a68ef872c1107
Daniel <danirvicente@gmail.com>
2022-08-23 17:24:39 +0200

    map and filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [countries, setCountries] = useState([])
+    const [name, setName] = useState("")
 
-export default App;
+    useEffect(() => {
+        axios.get("https//restcountries.com/v3.1/all").then((response) => {
+            setCountries(
+                response.data.map(({ name, capital, area, languages, flags }) => ({
+                    name: name.common,
+                    capital,
+                    area,
+                    languages,
+                    flags
+                }))
+            )
+        })
+    }, [])
+
+    const handleChange = (event) => setName(event.target.value)
+
+    const filterCountries = countries.filter((country) => country.name.includes(name))
+
+    return (
+        <div>
+            <p>
+                find countries
+                <input value={name} onChange={handleChange} />
+            </p>
+        </div>
+    )
+}
+export default App
